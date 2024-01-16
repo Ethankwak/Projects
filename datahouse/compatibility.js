a86985cdb4ea36283521cf60005841a7d19929e8
ethankwak <96744ejk@gmail.com>
2024-01-15 18:21:24 -0800

    finished

--- datahouse/compatibility.js
+++ datahouse/compatibility.js
@@ -1,28 +1,75 @@
 const fs = require('fs');
 
 const fileContent = fs.readFileSync('input.json', 'utf-8');
-const data = JSON.parse(fileContent);
+const input = JSON.parse(fileContent);
 
-console.log(data);
-
-let teamIntel  ligence = 0;
-let teamStrength = 0;
-let teamEndurance = 0;
-let teamCharisma = 0;
+const teamTraits = {
+    intelligence: 0,
+    strength: 0,
+    endurance: 0,
+    charisma: 0
+};
 
 //add up total scores of the current team to see the strengths and weaknesses
-for (let i = 0; i < data.team.length; i++ ) {
-    teamIntelligence += data.team[i].attributes.intelligence;
-    teamStrength += data.team[i].attributes.strength;
-    teamEndurance += data.team[i].attributes.endurance;
-    teamCharisma += data.team[i].attributes.charisma;
+for (const team of input.team) {
+    teamTraits.intelligence += team.attributes.intelligence;
+    teamTraits.strength += team.attributes.strength;
+    teamTraits.endurance += team.attributes.endurance;
+    teamTraits.charisma += team.attributes.charisma;
 }
 
-const teamTraits[teamIntelligence, teamStrength, teamEndurance, teamCharisma];
-teamTraits.sort(a,b) => b - a);
+//find the weakest trait on the team
+let minValue = Infinity;
+let minTrait;
 
-let greatestNeed = 
-for (let i = 0; i < data.applicants.length; i++ ) {
-
+//loop that iterates through teamTraits object and sets lowest trait value into variable as well as the name of the trait
+for (const trait in teamTraits) {
+    if(teamTraits.hasOwnProperty(trait)) {
+        if(teamTraits[trait] < minValue) {
+            minValue = teamTraits[trait];
+            minTrait = trait;
+        }
+    }
 }
 
+// object to store applicants
+const result = {
+    scoredApplicants: []
+
+};
+
+//compare which applicant is strongest in the needed category as
+let firstApplicant = input.applicants[0];
+let bestApplicant;
+let maxTrait = firstApplicant.attributes[minTrait];
+
+for (applicant of input.applicants) {
+
+    const totalScore = applicant.attributes.intelligence +
+    applicant.attributes.strength + 
+    applicant.attributes.endurance + 
+    applicant.attributes.charisma;
+
+    //scoring system so that if an applicant had 10/10/10/10 traits it would add up to 1
+    let score =  (totalScore * 1/4)/10;
+
+    //bonus points given to an applicant with the highest value in trait the team needs the most
+    if (applicant.attributes[minTrait] > maxTrait) {
+        bestApplicant = applicant;
+        maxTrait = applicant.attributes[minTrait];
+        tieApplicants = [];
+        score += 0.2;
+    }
+
+    result.scoredApplicants.push({
+        name: applicant.name,
+        score: score
+    });
+}
+
+
+
+const jsonOutput = JSON.stringify(result, null, 2); // The third argument (2) is for indentation
+console.log(jsonOutput);
+fs.writeFileSync('output.json', jsonOutput);
+console.log('Output saved to output.json');
